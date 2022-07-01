76bad90cad669f01e9ed67737119439d1f9415c5
etoscano <eleonora.toscano@mail.polimi.it>
2022-07-01 12:29:04 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./styles/main.scss";
+
+import {Home} from './pages/Home/Home.jsx'
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <Home></Home>
+  );
+}
+
+export default App;
