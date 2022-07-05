8e1fb405d9dfd48fc9d161e58f43aebe6dc1c034
etoscano <eleonora.toscano@mail.polimi.it>
2022-07-05 12:09:13 +0200

    footer p6

--- src/modules/Car/Car.jsx
+++ src/modules/Car/Car.jsx
@@ -1,6 +1,144 @@
-
+import React , {useEffect} from 'react';
 
 function Car() {
+  var mouse = { x: 0, y: 0 }; //mouse.x, mouse.y
+  // var width = document.documentElement.clientWidth;
+  var width = 1440;
+  var carpos = { x: 0, y: 0 };
+  var dir = "normal";
+  var car = document.getElementsByClassName("js-footer__car")[0];
+
+  useEffect(() => {
+
+    var container = document.getElementsByClassName("js-footer__container")[0];
+    container.addEventListener('click', myFunction);
+    document.addEventListener("mousemove", getMouse);
+
+    const interval = setInterval(() => {
+      followMouse()
+    }, 50);
+
+
+    return () => clearInterval(interval);
+  }, []);
+
+
+
+  function getMouse(e) {
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
+  
+    //Checking directional change
+    if (mouse.x < width / 15) {
+      dir = "left7";
+    } else if (mouse.x < (width * 2) / 15) {
+      dir = "left6";
+    } else if (mouse.x < (width * 3) / 15) {
+      dir = "left5";
+    } else if (mouse.x < (width * 4) / 15) {
+      dir = "left4";
+    } else if (mouse.x < (width * 5) / 15) {
+      dir = "left3";
+    } else if (mouse.x < (width * 6) / 15) {
+      dir = "left2";
+    } else if (mouse.x < (width * 7) / 15) {
+      dir = "left1";
+    } else if (mouse.x < (width * 8) / 15) {
+      dir = "normal";
+    } else if (mouse.x < (width * 9) / 15) {
+      dir = "right1";
+    } else if (mouse.x < (width * 10) / 15) {
+      dir = "right2";
+    } else if (mouse.x < (width * 11) / 15) {
+      dir = "right3";
+    } else if (mouse.x < (width * 12) / 15) {
+      dir = "right4";
+    } else if (mouse.x < (width * 13) / 15) {
+      dir = "right5";
+    } else if (mouse.x < (width * 14) / 15) {
+      dir = "right6";
+    } else {
+      dir = "right7";
+    }
+  
+  }
+  
+  function followMouse() {
+
+    var car = document.getElementsByClassName("js-footer__car")[0];
+  
+    //1. find distance X , distance Y
+    var lambda = 0.6;
+    var lambda2 = 0.2 * width;
+  
+    var distX = mouse.x * lambda + lambda2 - carpos.x;
+    // var distY = mouse.y * lambda + lambda2 - carpos.y;
+  
+    var w = car.clientWidth;
+    //Easing motion
+    //Progressive reduction of distance
+    carpos.x += distX / 5 - 0.1 * w;
+    // carpos.y += distY/2;
+  
+    car.style.left = carpos.x + "px";
+    // car.style.top = carpos.y + "px";
+  
+    var imgcar = document.getElementsByClassName("js-footer__img")[0];
+  
+    //Apply css class
+    if (dir == "right7") {
+      imgcar.src = "./CarR7.png";
+    } else if (dir == "right6") {
+      imgcar.src = "./CarR6.png";
+    } else if (dir == "right5") {
+      imgcar.src = "./CarR5.png";
+    } else if (dir == "right4") {
+      imgcar.src = "./CarR4.png";
+    } else if (dir == "right3") {
+      imgcar.src = "./CarR3.png";
+    } else if (dir == "right2") {
+      imgcar.src = "./CarR2.png";
+    } else if (dir == "right1") {
+      imgcar.src = "./CarR1.png";
+    } else if (dir == "normal") {
+      imgcar.src = "./Car1.png";
+    } else if (dir == "left1") {
+      imgcar.src = "./CarL1.png";
+    } else if (dir == "left2") {
+      imgcar.src = "./CarL2.png";
+    } else if (dir == "left3") {
+      imgcar.src = "./CarL3.png";
+    } else if (dir == "left4") {
+      imgcar.src = "./CarL4.png";
+    } else if (dir == "left5") {
+      imgcar.src = "./CarL5.png";
+    } else if (dir == "left6") {
+      imgcar.src = "./CarL6.png";
+    } else if (dir == "left7") {
+      imgcar.src = "./CarL7.png";
+    }
+  
+  }
+  
+  function myFunction() {
+    // var sun = document.getElementById("sun");
+    // sun.classList.toggle("sun");
+    // sun.classList.toggle("sun2");
+  
+    var m1 = document.getElementsByClassName("js-footer__mountains__m1")[0];
+    var m2 = document.getElementsByClassName("js-footer__mountains__m2")[0];
+    var m3 = document.getElementsByClassName("js-footer__mountains__m3")[0];
+    m1.classList.toggle("footer__mountains__m1");
+    m2.classList.toggle("footer__mountains__m2");
+    m3.classList.toggle("footer__mountains__m3");
+  
+    var bottombg = document.getElementsByClassName("js-footer__bg")[0];
+    bottombg.classList.toggle("footer__road--night");
+  }
+
+
+
+
   return (
 
 
@@ -35,130 +173,3 @@ function Car() {
 export default Car;
 
 
-var car = document.getElementsByClassName("js-footer__car")[0];
-
-if(car){
-var container = document.getElementsByClassName("js-footer__container")[0];
-container.addEventListener('click', myFunction);
-
-var imgcar = document.getElementsByClassName("js-footer__img")[0];
-document.addEventListener("mousemove", getMouse);
-
-var width = document.documentElement.clientWidth;
-
-// car.style.position = "relative"; //css
-var carpos = { x: 0, y: 0 };
-
-setInterval(followMouse, 50);
-
-var mouse = { x: 0, y: 0 }; //mouse.x, mouse.y
-
-var dir = "normal";
-
-function getMouse(e) {
-  mouse.x = e.pageX;
-  mouse.y = e.pageY;
-
-  //Checking directional change
-  if (mouse.x < width / 15) {
-    dir = "left7";
-  } else if (mouse.x < (width * 2) / 15) {
-    dir = "left6";
-  } else if (mouse.x < (width * 3) / 15) {
-    dir = "left5";
-  } else if (mouse.x < (width * 4) / 15) {
-    dir = "left4";
-  } else if (mouse.x < (width * 5) / 15) {
-    dir = "left3";
-  } else if (mouse.x < (width * 6) / 15) {
-    dir = "left2";
-  } else if (mouse.x < (width * 7) / 15) {
-    dir = "left1";
-  } else if (mouse.x < (width * 8) / 15) {
-    dir = "normal";
-  } else if (mouse.x < (width * 9) / 15) {
-    dir = "right1";
-  } else if (mouse.x < (width * 10) / 15) {
-    dir = "right2";
-  } else if (mouse.x < (width * 11) / 15) {
-    dir = "right3";
-  } else if (mouse.x < (width * 12) / 15) {
-    dir = "right4";
-  } else if (mouse.x < (width * 13) / 15) {
-    dir = "right5";
-  } else if (mouse.x < (width * 14) / 15) {
-    dir = "right6";
-  } else {
-    dir = "right7";
-  }
-
-}
-
-function followMouse() {
-  //1. find distance X , distance Y
-  var lambda = 0.6;
-  var lambda2 = 0.1 * width;
-
-  var distX = mouse.x * lambda + lambda2 - carpos.x;
-  var distY = mouse.y * lambda + lambda2 - carpos.y;
-
-  var w = car.clientWidth;
-  //Easing motion
-  //Progressive reduction of distance
-  carpos.x += distX / 5 - 0.1 * w;
-  // carpos.y += distY/2;
-
-  car.style.left = carpos.x + "px";
-  // car.style.top = carpos.y + "px";
-
-  //Apply css class
-  if (dir == "right7") {
-    imgcar.src = "./CarR7.png";
-  } else if (dir == "right6") {
-    imgcar.src = "./CarR6.png";
-  } else if (dir == "right5") {
-    imgcar.src = "./CarR5.png";
-  } else if (dir == "right4") {
-    imgcar.src = "./CarR4.png";
-  } else if (dir == "right3") {
-    imgcar.src = "./CarR3.png";
-  } else if (dir == "right2") {
-    imgcar.src = "./CarR2.png";
-  } else if (dir == "right1") {
-    imgcar.src = "./CarR1.png";
-  } else if (dir == "normal") {
-    imgcar.src = "./Car1.png";
-  } else if (dir == "left1") {
-    imgcar.src = "./CarL1.png";
-  } else if (dir == "left2") {
-    imgcar.src = "./CarL2.png";
-  } else if (dir == "left3") {
-    imgcar.src = "./CarL3.png";
-  } else if (dir == "left4") {
-    imgcar.src = "./CarL4.png";
-  } else if (dir == "left5") {
-    imgcar.src = "./CarL5.png";
-  } else if (dir == "left6") {
-    imgcar.src = "./CarL6.png";
-  } else if (dir == "left7") {
-    imgcar.src = "./CarL7.png";
-  }
-
-}
-
-function myFunction() {
-  // var sun = document.getElementById("sun");
-  // sun.classList.toggle("sun");
-  // sun.classList.toggle("sun2");
-
-  var m1 = document.getElementsByClassName("js-footer__mountains__m1")[0];
-  var m2 = document.getElementsByClassName("js-footer__mountains__m2")[0];
-  var m3 = document.getElementsByClassName("js-footer__mountains__m3")[0];
-  m1.classList.toggle("footer__mountains__m1");
-  m2.classList.toggle("footer__mountains__m2");
-  m3.classList.toggle("footer__mountains__m3");
-
-  var bottombg = document.getElementsByClassName("js-footer__bg")[0];
-  bottombg.classList.toggle("footer__road--night");
-}
-}
