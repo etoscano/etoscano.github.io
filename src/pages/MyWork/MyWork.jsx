3b7e865188c6126b9d9eee771d806409c15bfdc2
etoscano <eleonora.toscano@mail.polimi.it>
2022-07-05 17:40:13 +0200

    my work p2

--- src/pages/MyWork/MyWork.jsx
+++ src/pages/MyWork/MyWork.jsx
@@ -16,25 +16,26 @@ function MyWork({  }) {
             <Column size={2}  ></Column>
             
             <Column size={8}  >
-              <h1 className='mywork__title'>My Work</h1>
-              
-              <div  className='mywork__project'>
-                <div  className='mywork__container'>
-                    <div  className='mywork__innerContainer'>
-                        <h2  className='mywork__projectTitle'>Project</h2>
-                        <img  className='mywork__img'></img>
+                <div className='mywork__wrapper'>
+                    <h1 className='mywork__title'>My Work</h1>
+                    
+                    <div  className='mywork__project'>
+                        <div  className='mywork__container'>
+                            <div  className='mywork__innerContainer'>
+                                <h2  className='mywork__projectTitle'>Project</h2>
+                                <img  className='mywork__img' src="./project_1.png" alt=""></img>
+                            </div>
+                        </div>
+
+                        <div className='mywork__info'>
+                            <p  className='mywork__year'>2021</p>
+                            <div className='mywork__techs'>
+                                <p  className='mywork__tech'>ThreeJS</p>
+                                <p  className='mywork__tech'>HTML/CSS</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
-                <div className='mywork__info'>
-                    <p  className='mywork__year'>2021</p>
-                    <div className='mywork__techs'>
-                        <p  className='mywork__tech'>ThreeJS</p>
-                        <p  className='mywork__tech'>HTML/CSS</p>
-                    </div>
-                </div>
-              </div>
-
               {/* <Router> */}
               {/* <Link className='mywork__link' to="/contact"> Contact me
               <img className="mywork__icon" src="./chat.svg" alt=""></img>
